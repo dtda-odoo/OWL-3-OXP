a5af4f56c5ea485db09fbc214db1807e1bfe6e86
dtda-odoo <dtda@odoo.com>
2026-07-19 19:36:26 +0530

    Component and Event Handling

--- todo/static/src/app.js
+++ todo/static/src/app.js
@@ -3,6 +3,14 @@ import { registry } from "@web/core/registry";
 
 export class TodoApp extends Component {
     static template = "odoo_todo.TodoApp";
+
+    setup() {
+        // Component initialization happens here.
+    }
+
+    onAddTodoClick() {
+        window.alert("Coming soon! We'll add todos in the next lesson.");
+    }
 }
 
 registry.category("actions").add("odoo_todo.todo_app", TodoApp);
